52a167d55539e6983e36304dd530d0c2ac1c376b
Nesarel <nesarel.gd@gmail.com>
2022-11-29 17:04:02 +0100

    comprehension twig

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,8 +13,23 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
+
+        // logique
+        // accès BDD
+        // calcul
+
+        $tableau = [
+
+            1 => ['nom' => 'bleu', 'code' => '#0000ff'],
+            2 => ['nom' => 'blanc', 'code' => '#ffffff'],
+            3 => ['nom' => 'rouge', 'code' => '#ff0000']
+
+        ];
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
+            'test' => 'Une valeur',
+            'liste' => $tableau,
         ]);
     }
 }

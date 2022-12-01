f852e755f9b8974b307f30c9003381febe312f84
Nesarel <nesarel.gd@gmail.com>
2022-12-01 12:32:43 +0100

    More fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,25 +2,59 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Ville;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Aeroport;
+use App\Entity\Company;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
 
-        $ville = new Ville();
+        $faker = Factory::create('fr_FR');
 
-        $ville
-            ->setNom('Paris')
-            ->setDepartement('75')
-            ->setPays('France')
-            ->setPopulation(2161000);
+        // Villes et Aéroports
+        for($i = 1; $i <= 5; $i++){
 
-        $manager->persist($ville);
+            $aeroport = new Aeroport;
+            $aeroport
+                ->setNom("Aéroport $i")
+                ->setNbPistes("$i");
+            $manager->persist($aeroport);
+
+            $ville = new Ville();
+            $ville
+                ->setNom($faker->city())
+                ->setDepartement($faker->departmentName())
+                ->setPays('France')
+                ->setPopulation($faker->randomNumber(6))
+                ->addAeroport($aeroport);
+
+            if($i % 2 == 0){
+                $aeroport = new Aeroport;
+                $aeroport
+                    ->setNom("Aéroport $i bis")
+                    ->setNbPistes("$i");
+                $manager->persist($aeroport);
+                $ville->addAeroport($aeroport);
+            }
+
+            $manager->persist($ville);
+
+        }
+
+        // Compagnies
+        for($i = 1; $i <= 5; $i++){
+            $company = new Company();
+            $company
+                ->setNom("Compagnie $i")
+                ->setSigle("C_$i")
+                ->setEmployes(mt_rand(50,1000));
+            $manager->persist($company);
+        }
 
         $manager->flush();
     }
